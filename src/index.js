57f9210c1f4fd46713224176309b20e5078cc8ab
redmirx <redmirx9@gmail.com>
2022-09-12 22:29:37 +0500

    React file updated

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     {/* <App /> */}
-    <p>Hi there</p>
+    <center sytle="background: red"></center>
   </React.StrictMode>
 );
 
