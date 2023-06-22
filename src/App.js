d80b1436c57f8fa64d8ca79503f845de11b370c5
redmirx <redmirx9@gmail.com>
2023-06-22 20:01:46 +0500

    new commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const App = () => {
+  navigator.usb.getDevices().then((devices) => {
+    console.log('hi');
+    devices.forEach((device) => {
+      console.log(device.productName); // "Arduino Micro"
+      console.log(device.manufacturerName); // "Arduino LLC"
+    });
+  });
+  return <h1>Scale</h1>;
+};
+
+export default App;
--- src/components/ProductCard/index.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import { Container } from './style.js';
-
-const ProductCard = ({ title }) => {
-  return <Container>{title}</Container>;
-};
-
-export default ProductCard;
