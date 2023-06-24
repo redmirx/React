7a91c51bb0360c875f66bd356348e13eedebac78
redmirx <redmirx9@gmail.com>
2023-06-24 11:37:13 +0500

    new commit

--- src/App.js
+++ src/App.js
@@ -1,15 +1,40 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const App = () => {
+  const [count, setCount] = useState(0);
+  // console.log(navigator.usb);
+  // navigator.usb.getDevices().then((devices) => {
+  //   console.log('hi');
+  //   console.log(devices);
+  //   devices.forEach((device) => {
+  //     console.log(device.productName); // "Arduino Micro"
+  //     console.log(device.manufacturerName); // "Arduino LLC"
+  //   });
+  // });
+
+  // Get devices
   navigator.usb.getDevices().then((devices) => {
-    console.log('hi');
-    console.log(devices);
+    console.log(`Total devices: ${devices.length}`);
+    setCount(devices.count);
     devices.forEach((device) => {
-      console.log(device.productName); // "Arduino Micro"
-      console.log(device.manufacturerName); // "Arduino LLC"
+      console.log(
+        `Product name: ${device.productName}, serial number ${device.serialNumber}`
+      );
     });
   });
-  return <h1>Scale</h1>;
+  console.log('-----------------------------');
+
+  // Request to Device
+  const filters = [{ vendorId: 0x8087, productId: 0x0a2a }];
+  navigator.usb
+    .requestDevice({ filters })
+    .then((usbDevice) => {
+      console.log(`Product name: ${usbDevice.productName}`);
+    })
+    .catch((err) => {
+      console.error(`There is no device. ${err}`);
+    });
+  return <h1>Devices found: {count || 0}</h1>;
 };
 
 export default App;
